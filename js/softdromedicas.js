549658a7595354cfbebae4e0ed0222ecb0137575
lfernandortiz <lfernandortiz@gmail.com>
2016-09-20 17:00:14 -0500

    editando el css de infowindows

--- js/softdromedicas.js
+++ js/softdromedicas.js
@@ -12,35 +12,36 @@ var lng= -72.49668959999997;
 
 //informacion y coordenada de sucursales
 var sucursales = [
-	['Dromedicas del Oriente SAS', 7.908388743984923, -72.491574883461, 'AVENIDA 11 BE # 8BN - 10  GUAIMARAL', '5740075','5777762', 'CUCUTA','','', '', '', '', 1],
-	['Farmanorte 01', 7.840764903473619, -72.5028133392334, 'CALLE 33 CON AVENIDA 4 ESQUINA BRR LA SABANA', '5808800','3167409253', 'LOS PATIOS','','7:30:00', '22:30:00', '7:30:00', '22:30:00', 2],
-	['Farmanorte 02', 7.923595410892432, -72.52201795578003, 'AVENIDA 5 CON CALLE 2N PESCADERO', '5780727','3166909962', 'CUCUTA','','8:00:00', '23:30:00', '', '', 3],
-	['Farmanorte 03', 7.917091999388589, -72.49572694301605, 'AVENIDA 4 CON CALLE 20AN ESQUINA BRR PRADOS DEL NORTE', '5796888','3166909583', 'CUCUTA','true', '', '', '', '', 4],
-	['Farmanorte 04', 7.9049350202970805, -72.51519441604614, 'AVENIDA KENNEDY CON 2DA ESQUINA BRR LA VICTORIA', '5787878','3183353570', 'CUCUTA','','7:30:00', '21:00:00', '7:30:00', '7:30:00', 5],
-	['Farmanorte 05', 7.898048740341691, -72.52727508544922, 'CALLE 2 CON AVENIDA 6 ESQUINA BRR CECI', '5870555','3168309523', 'CUCUTA','true', '', '', '', '', 6],
-	['Farmanorte 06', 7.87261944, -72.52802511, 'AVENIDA 26 CON CALLE 28 ESQUINA BRR BELEN', '5828280','3155888094', 'CUCUTA','true', '', '', '', '', 7],
-	['Farmanorte 07', 7.8904609470678055, -72.49629020690918, 'CALLE 8 # 1E-76 JUNTO CAFESALUD LA SALLE', '5744094','3153437725', 'CUCUTA','true', '', '', '', '', 8],
-	['Farmanorte 08', 7.373990247532279, -72.64909029006958, 'CRR 6 # 7-99 CALLE REAL', '5682217','3183437726', 'PAMPLONA','','7:30:00', '22:00:00', '7:30:00', '7:30:00', 9],
-	['Farmanorte 09', 7.929875568608961, -72.50390768051147, 'AVENIDA 2 CON CALLE 4 ESQUINA BRR AEROPUERTO', '5818245','3166910544', 'CUCUTA','true', '', '', '', '', 10],
-	['Farmanorte 10', 7.912830721471756, -72.48752474784851, 'AVENIDA LIBERTADORES # 18N-181 BRR SANTA ELENA BLOQUES DEL ZULIMA', '5776206','3155997099', 'CUCUTA','','7:30:00', '22:00:00', '7:30:00', '7:30:00', 11],
-	['Farmanorte 11', 7.8871853, -72.49622654, 'CLL 11 CON AV 2E ESQUINA DIAGONAL C.C. VENTURA PLAZA', '5711737','3173643955', 'CUCUTA','true', '', '', '', '', 12],
-	['Farmanorte 12', 7.88983925194856, -72.49102771282196, 'AV. 9E CON CLL 9 ESQ FTE COL. DOMINGO SAVIO BRR LA RIVIERA', '5751377','3185166993', 'CUCUTA','','7:00:00', '22:00:00', '7:00:00', '7:00:00', 13],
-	['Farmanorte 13', 7.886720134976074, -72.51558065414429, 'AV 3E # 1-108 LOCAL 1 FRENTE AL PARQUE LA CEIBA', '577770','3185166990', 'CUCUTA','','8:00:00', '21:00:00', '8:00:00', '8:00:00', 14],
-	['Farmanorte 14', 7.88912722, -72.51350731, 'CLL 6 # 13-61 FTE ESE LOMA BOLIVAR', '5739782','3155120028', 'CUCUTA','true', '', '', '', '', 15],
-	['Farmanorte 15', 7.91121118, -72.51772667, 'AVENIDA 0 # 4-68 ESQUINA BARRIO COMUNEROS', '5796173','3154372258', 'CUCUTA','true', '', '', '', '', 16],
-	['Farmanorte 16', 7.607999367664449, -72.59926557540894, 'CLL 3 # 3-34  PARQ PRPAL CHINACOTA', '5864324','3173660022', 'CHINACOTA','true', '', '', '', '', 17],
-	['Farmanorte 17', 7.92706246, -72.51931541, 'AVENIDA 5 CON CALLE 24 OSPINA PEREZ ESQUINA', '5781108','3153084801', 'CUCUTA','true', '', '', '', '', 18],
-	['Farmanorte 18', 7.88198692, -72.4986197, 'AVENDAD 1 #15-87 BARRIO LA PLAYA', '5710546','3176410633', 'CUCUTA','','7:00:00', '21:30:00', '8:00:00', '8:00:00', 19],
-	['Farmanorte 19', 7.87879545, -72.49714686, 'AVENIDA 0 CON CALLE 19 ESQUINA', '5831554','3174378532', 'CUCUTA','true', '', '', '', '', 20],
-	['Farmanorte 20', 7.88715719, -72.5027738, 'CALLE 10 CON AVENIDA 4 ESQUINA', '5730318','3175020642', 'CUCUTA','','7:30:00', '20:00:00', '8:00:00', '8:00:00', 21],
-	['Farmanorte 21', 7.88680822, -72.5048961, 'AV 6 CON CALLE 10# 5-94  FRENTE AL PARQUE SANTANDER CENTRO', '5719113','3174380134', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 22],
-	['Farmanorte 22', 8.23596841, -73.35418615, 'CALLE 10 # 12-87 FRENTE AL PARQUE - OCAÑA', '5691839','3153926643', 'OCAÑA','','7:30:00', '21:00:00', '7:30:00', '7:30:00', 23],
-	['Farmanorte 23', 7.88755082, -72.50502387, 'AVENIDA 6 # 9-21', '5730526','3183380633', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 24],
-	['Farmanorte 24', 7.88765518, -72.49848544, 'AVENIDA 0 · 10-14', '5711114','3175021518', 'CUCUTA','true', '', '', '', '', 25],
-	['Farmanorte 25', 7.8666476, -72.49764258, 'CENTRO CIAL PINAR DEL RIO LOCAL 10', '5842555','3174333572', 'CUCUTA','true', '', '', '', '', 26],
-	['Farmanorte 26', 7.87187112, -72.52892628, 'AVENIDA 26 NO 29-48 BR BELEN', '5754140','3162331144', 'CUCUTA','','7:30:00', '21:30:00', '7:30:00', '7:30:00', 27],
-	['Farmanorte 27', 7.91694492, -72.4727475, 'EL ESCOBAL, ANILLO VIAL, PLAZA DEL ESTE LOCAL 6', '5847808','3188135356', 'CUCUTA','','7:00:00', '22:00:00', '7:00:00', '7:00:00', 28],
-	['San Antonio del Norte', 7.88749215, -72.50609315, 'AV 7 CALLE 9 ESQUINA CENTRO', '5727091','3155997098', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 29],
+	['Dromedicas del Oriente SAS', 7.908388743984923, -72.491574883461, 'Avenida 11 Be # 8Bn - 10  Guaimaral', '5740075','5777762', 'CUCUTA','','', '', '', '', 1],
+	['Farmanorte 01', 7.840764903473619, -72.5028133392334, 'Calle 33 Con Avenida 4 Esquina Brr La Sabana', '5808800','3167409253', 'LOS PATIOS','','7:30:00', '22:30:00', '7:30:00', '22:30:00', 2],
+	['Farmanorte 02', 7.923595410892432, -72.52201795578003, 'Avenida 5 Con Calle 2N Pescadero', '5780727','3166909962', 'CUCUTA','','8:00:00', '23:30:00', '', '', 3],
+	['Farmanorte 03', 7.917091999388589, -72.49572694301605, 'Avenida 4 Con Calle 20An Esquina Brr Prados Del Norte', '5796888','3166909583', 'CUCUTA','true', '', '', '', '', 4],
+	['Farmanorte 04', 7.9049350202970805, -72.51519441604614, 'Avenida Kennedy Con 2Da Esquina Brr La Victoria', '5787878','3183353570', 'CUCUTA','','7:30:00', '21:00:00', '7:30:00', '7:30:00', 5],
+	['Farmanorte 05', 7.898048740341691, -72.52727508544922, 'Calle 2 Con Avenida 6 Esquina Brr Ceci', '5870555','3168309523', 'CUCUTA','true', '', '', '', '', 6],
+	['Farmanorte 06', 7.87261944, -72.52802511, 'Avenida 26 Con Calle 28 Esquina Brr Belen', '5828280','3155888094', 'CUCUTA','true', '', '', '', '', 7],
+	['Farmanorte 07', 7.8904609470678055, -72.49629020690918, 'Calle 8 # 1E-76 Junto Cafesalud La Salle', '5744094','3153437725', 'CUCUTA','true', '', '', '', '', 8],
+	['Farmanorte 08', 7.373990247532279, -72.64909029006958, 'Crr 6 # 7-99 Calle Real', '5682217','3183437726', 'PAMPLONA','','7:30:00', '22:00:00', '7:30:00', '7:30:00', 9],
+	['Farmanorte 09', 7.929875568608961, -72.50390768051147, 'Avenida 2 Con Calle 4 Esquina Brr Aeropuerto', '5818245','3166910544', 'CUCUTA','true', '', '', '', '', 10],
+	['Farmanorte 10', 7.912830721471756, -72.48752474784851, 'Avenida Libertadores # 18N-181 Brr Santa Elena Bloques Del Zulima', '5776206','3155997099', 'CUCUTA','','7:30:00', '22:00:00', '7:30:00', '7:30:00', 11],
+	['Farmanorte 11', 7.8871853, -72.49622654, 'Cll 11 Con Av 2E Esquina Diagonal C.C. Ventura Plaza', '5711737','3173643955', 'CUCUTA','true', '', '', '', '', 12],
+	['Farmanorte 12', 7.88983925194856, -72.49102771282196, 'Av. 9E Con Cll 9 Esq Fte Col. Domingo Savio Brr La Riviera', '5751377','3185166993', 'CUCUTA','','7:00:00', '22:00:00', '7:00:00', '7:00:00', 13],
+	['Farmanorte 13', 7.886720134976074, -72.51558065414429, 'Av 3E # 1-108 Local 1 Frente Al Parque La Ceiba', '577770','3185166990', 'CUCUTA','','8:00:00', '21:00:00', '8:00:00', '8:00:00', 14],
+	['Farmanorte 14', 7.88912722, -72.51350731, 'Cll 6 # 13-61 Fte Ese Loma Bolivar', '5739782','3155120028', 'CUCUTA','true', '', '', '', '', 15],
+	['Farmanorte 15', 7.91121118, -72.51772667, 'Avenida 0 # 4-68 Esquina Barrio Comuneros', '5796173','3154372258', 'CUCUTA','true', '', '', '', '', 16],
+	['Farmanorte 16', 7.607999367664449, -72.59926557540894, 'Cll 3 # 3-34  Parq Prpal Chinacota', '5864324','3173660022', 'CHINACOTA','true', '', '', '', '', 17],
+	['Farmanorte 17', 7.92706246, -72.51931541, 'Avenida 5 Con Calle 24 Ospina Perez Esquina', '5781108','3153084801', 'CUCUTA','true', '', '', '', '', 18],
+	['Farmanorte 18', 7.88198692, -72.4986197, 'Avendad 1 #15-87 Barrio La Playa', '5710546','3176410633', 'CUCUTA','','7:00:00', '21:30:00', '8:00:00', '8:00:00', 19],
+	['Farmanorte 19', 7.87879545, -72.49714686, 'Avenida 0 Con Calle 19 Esquina', '5831554','3174378532', 'CUCUTA','true', '', '', '', '', 20],
+	['Farmanorte 20', 7.88715719, -72.5027738, 'Calle 10 Con Avenida 4 Esquina', '5730318','3175020642', 'CUCUTA','','7:30:00', '20:00:00', '8:00:00', '8:00:00', 21],
+	['Farmanorte 21', 7.88680822, -72.5048961, 'Av 6 Con Calle 10# 5-94  Frente Al Parque Santander Centro', '5719113','3174380134', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 22],
+	['Farmanorte 22', 8.23596841, -73.35418615, 'Calle 10 # 12-87 Frente Al Parque - Ocaña', '5691839','3153926643', 'OCAÑA','','7:30:00', '21:00:00', '7:30:00', '7:30:00', 23],
+	['Farmanorte 23', 7.88755082, -72.50502387, 'Avenida 6 # 9-21', '5730526','3183380633', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 24],
+	['Farmanorte 24', 7.88765518, -72.49848544, 'Avenida 0 · 10-14', '5711114','3175021518', 'CUCUTA','true', '', '', '', '', 25],
+	['Farmanorte 25', 7.8666476, -72.49764258, 'Centro Cial Pinar Del Rio Local 10', '5842555','3174333572', 'CUCUTA','true', '', '', '', '', 26],
+	['Farmanorte 26', 7.87187112, -72.52892628, 'Avenida 26 No 29-48 Br Belen', '5754140','3162331144', 'CUCUTA','','7:30:00', '21:30:00', '7:30:00', '7:30:00', 27],
+	['Farmanorte 27', 7.91694492, -72.4727475, 'El Escobal, Anillo Vial, Plaza Del Este Local 6', '5847808','3188135356', 'CUCUTA','','7:00:00', '22:00:00', '7:00:00', '7:00:00', 28],
+	['San Antonio del Norte', 7.88749215, -72.50609315, '', '5727091','3155997098', 'CUCUTA','','7:00:00', '20:00:00', '8:00:00', '8:00:00', 29],
+
 ];
 
 //metodo principa
@@ -120,8 +121,9 @@ function addMarkerWithTimeout(position, timeout, suc, i, dir, telefono, celular,
 						'<div class="row-content"><span class="icon-home"></span><div class="infocontent">'+ dir +'</div></div>'+
 						'<div class="row-content"><a href="tel:(037)'+ telefono +'" class="footertext"><span class="icon-phone"></span><span class="infocontent">'+ telefono +'</span></a></div>'+	
 						'<div class="row-content"><a href="tel:'+ celular +'" class="footertext"><span class="icon-mobile"></span><span class="infocontent">'+celular+'</span></a></div>'+	
+						'<div class="row-content final"></div>'	+					
 					'<div class="row-content">'+
-						'<div class="titlehorario"><h3>Horarios</h3></div>'+
+						'<div class="titlesection"><h3>Horario</h3></div>'+
 						'<div class="horariocontent"><div class="estado"></div></div>'+
 						'<div class="rutacontent"><div class="titlehorario"></div><div class="buttoncontent"></div></div>'
 					'</div><!--fin de contenedor de horarios -->'+
@@ -270,7 +272,7 @@ function editCssInfoWindow(){
 		* and took advantage of the existing reference .gm-style-iw for the previous div with .prev().
 		*/		
 		var iwBackground = iwOuter.prev();
-		iwBackground.children(':nth-child(1)').css({'display' : 'block'});
+		iwOuter.children(':nth-child(1)').css({'display' : 'block'});
 		// Removes background shadow DIV
 		iwBackground.children(':nth-child(2)').css({'display' : 'none'});
 		// Removes white background DIV
