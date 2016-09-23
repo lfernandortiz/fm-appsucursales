877f20db0adce083efdd12eb8bc6314df84a6e84
lfernandortiz <lfernandortiz@gmail.com>
2016-09-23 11:30:54 -0500

    probando semaforizacion de estilos

--- js/softdromedicas.js
+++ js/softdromedicas.js
@@ -3,7 +3,7 @@ console.log("test GMaps");
 var map;
 var markers = [];
 //varible para objeto de informacion del marcador 
-var infowindow;
+var infoWindowCustom;
 
 //coordenadas iniciales
 var lat=  7.8890971;
@@ -12,37 +12,35 @@ var lng= -72.49668959999997;
 
 //informacion y coordenada de sucursales
 var sucursales = [
-	['Dromedicas del Oriente SAS', 7.908388743984923, -72.491574883461, 'Avenida 11 Be # 8Bn - 10  Guaimaral', '5740075','5777762', 'CUCUTA','',, , , , 1],
-	['Farmanorte 01', 7.840764903473619, -72.5028133392334, 'Calle 33 Con Avenida 4 Esquina Brr La Sabana', '5808800','3167409253', 'LOS PATIOS','','7:30', '22:30', '7:30', '22:30', 2],
-	['Farmanorte 02', 7.923595410892432, -72.52201795578003, 'Avenida 5 Con Calle 2N Pescadero', '5780727','3166909962', 'CUCUTA','','8', '23:30', '', '', 3],
+	['Dromedicas del Oriente SAS', 7.908388743984923, -72.491574883461, 'Avenida 11 Be # 8Bn - 10  Guaimaral', '5740075','5777762', 'CUCUTA','','', '', '', '', 1],
+	['Farmanorte 01', 7.840764903473619, -72.5028133392334, 'Calle 33 Con Avenida 4 Esquina Brr La Sabana', '5808800','3167409253', 'LOS PATIOS','','11:30am', '22:30', '7:30am', '22:30', 2],
+	['Farmanorte 02', 7.923595410892432, -72.52201795578003, 'Avenida 5 Con Calle 2N Pescadero', '5780727','3166909962', 'CUCUTA','','8am', '23:30', '8am', '2pm', 3],
 	['Farmanorte 03', 7.917091999388589, -72.49572694301605, 'Avenida 4 Con Calle 20An Esquina Brr Prados Del Norte', '5796888','3166909583', 'CUCUTA','true', '', '', '', '', 4],
-	['Farmanorte 04', 7.9049350202970805, -72.51519441604614, 'Avenida Kennedy Con 2Da Esquina Brr La Victoria', '5787878','3183353570', 'CUCUTA','','7:30', '21', '7:30', '7:30', 5],
+	['Farmanorte 04', 7.9049350202970805, -72.51519441604614, 'Avenida Kennedy Con 2Da Esquina Brr La Victoria', '5787878','3183353570', 'CUCUTA','','7:30am', '21', '7:30am', '9pm', 5],
 	['Farmanorte 05', 7.898048740341691, -72.52727508544922, 'Calle 2 Con Avenida 6 Esquina Brr Ceci', '5870555','3168309523', 'CUCUTA','true', '', '', '', '', 6],
 	['Farmanorte 06', 7.87261944, -72.52802511, 'Avenida 26 Con Calle 28 Esquina Brr Belen', '5828280','3155888094', 'CUCUTA','true', '', '', '', '', 7],
 	['Farmanorte 07', 7.8904609470678055, -72.49629020690918, 'Calle 8 # 1E-76 Junto Cafesalud La Salle', '5744094','3153437725', 'CUCUTA','true', '', '', '', '', 8],
-	['Farmanorte 08', 7.373990247532279, -72.64909029006958, 'Crr 6 # 7-99 Calle Real', '5682217','3183437726', 'PAMPLONA','','7:30', '22', '7:30', '7:30', 9],
+	['Farmanorte 08', 7.373990247532279, -72.64909029006958, 'Crr 6 # 7-99 Calle Real', '5682217','3183437726', 'PAMPLONA','','7:30am', '22', '7:30am', '10pm', 9],
 	['Farmanorte 09', 7.929875568608961, -72.50390768051147, 'Avenida 2 Con Calle 4 Esquina Brr Aeropuerto', '5818245','3166910544', 'CUCUTA','true', '', '', '', '', 10],
-	['Farmanorte 10', 7.912830721471756, -72.48752474784851, 'Avenida Libertadores # 18N-181 Brr Santa Elena Bloques Del Zulima', '5776206','3155997099', 'CUCUTA','','7:30', '22', '7:30', '7:30', 11],
+	['Farmanorte 10', 7.912830721471756, -72.48752474784851, 'Avenida Libertadores # 18N-181 Brr Santa Elena Bloques Del Zulima', '5776206','3155997099', 'CUCUTA','','7:30am', '22', '7:30am', '9pm', 11],
 	['Farmanorte 11', 7.8871853, -72.49622654, 'Cll 11 Con Av 2E Esquina Diagonal C.C. Ventura Plaza', '5711737','3173643955', 'CUCUTA','true', '', '', '', '', 12],
-	['Farmanorte 12', 7.88983925194856, -72.49102771282196, 'Av. 9E Con Cll 9 Esq Fte Col. Domingo Savio Brr La Riviera', '5751377','3185166993', 'CUCUTA','','7', '22', '7', '7', 13],
-	['Farmanorte 13', 7.886720134976074, -72.51558065414429, 'Av 3E # 1-108 Local 1 Frente Al Parque La Ceiba', '577770','3185166990', 'CUCUTA','','8', '21', '8', '8', 14],
+	['Farmanorte 12', 7.88983925194856, -72.49102771282196, 'Av. 9E Con Cll 9 Esq Fte Col. Domingo Savio Brr La Riviera', '5751377','3185166993', 'CUCUTA','','7am', '22', '7am', '10pm', 13],
+	['Farmanorte 13', 7.886720134976074, -72.51558065414429, 'Av 3E # 1-108 Local 1 Frente Al Parque La Ceiba', '577770','3185166990', 'CUCUTA','','8am', '21', '8am', '9pm', 14],
 	['Farmanorte 14', 7.88912722, -72.51350731, 'Cll 6 # 13-61 Fte Ese Loma Bolivar', '5739782','3155120028', 'CUCUTA','true', '', '', '', '', 15],
 	['Farmanorte 15', 7.91121118, -72.51772667, 'Avenida 0 # 4-68 Esquina Barrio Comuneros', '5796173','3154372258', 'CUCUTA','true', '', '', '', '', 16],
 	['Farmanorte 16', 7.607999367664449, -72.59926557540894, 'Cll 3 # 3-34  Parq Prpal Chinacota', '5864324','3173660022', 'CHINACOTA','true', '', '', '', '', 17],
 	['Farmanorte 17', 7.92706246, -72.51931541, 'Avenida 5 Con Calle 24 Ospina Perez Esquina', '5781108','3153084801', 'CUCUTA','true', '', '', '', '', 18],
-	['Farmanorte 18', 7.88198692, -72.4986197, 'Avendad 1 #15-87 Barrio La Playa', '5710546','3176410633', 'CUCUTA','','7', '21:30', '8', '8', 19],
+	['Farmanorte 18', 7.88198692, -72.4986197, 'Avendad 1 #15-87 Barrio La Playa', '5710546','3176410633', 'CUCUTA','','7am', '21:30', '8am', '2pm', 19],
 	['Farmanorte 19', 7.87879545, -72.49714686, 'Avenida 0 Con Calle 19 Esquina', '5831554','3174378532', 'CUCUTA','true', '', '', '', '', 20],
-	['Farmanorte 20', 7.88715719, -72.5027738, 'Calle 10 Con Avenida 4 Esquina', '5730318','3175020642', 'CUCUTA','','7:30', '20', '8', '8', 21],
-	['Farmanorte 21', 7.88680822, -72.5048961, 'Av 6 Con Calle 10# 5-94  Frente Al Parque Santander Centro', '5719113','3174380134', 'CUCUTA','','7', '20', '8', '8', 22],
-	['Farmanorte 22', 8.23596841, -73.35418615, 'Calle 10 # 12-87 Frente Al Parque - Ocaña', '5691839','3153926643', 'OCAÑA','','7:30', '21', '7:30', '7:30', 23],
-	['Farmanorte 23', 7.88755082, -72.50502387, 'Avenida 6 # 9-21', '5730526','3183380633', 'CUCUTA','','7', '20', '8', '8', 24],
+	['Farmanorte 20', 7.88715719, -72.5027738, 'Calle 10 Con Avenida 4 Esquina', '5730318','3175020642', 'CUCUTA','','7:30am', '20', '8am', '2pm', 21],
+	['Farmanorte 21', 7.88680822, -72.5048961, 'Av 6 Con Calle 10# 5-94  Frente Al Parque Santander Centro', '5719113','3174380134', 'CUCUTA','','7am', '20', '8am', '2pm', 22],
+	['Farmanorte 22', 8.23596841, -73.35418615, 'Calle 10 # 12-87 Frente Al Parque - Ocaña', '5691839','3153926643', 'OCAÑA','','7:30am', '21', '7:30am', '5pm', 23],
+	['Farmanorte 23', 7.88755082, -72.50502387, 'Avenida 6 # 9-21', '5730526','3183380633', 'CUCUTA','','7am', '20', '8am', '2pm', 24],
 	['Farmanorte 24', 7.88765518, -72.49848544, 'Avenida 0 · 10-14', '5711114','3175021518', 'CUCUTA','true', '', '', '', '', 25],
 	['Farmanorte 25', 7.8666476, -72.49764258, 'Centro Cial Pinar Del Rio Local 10', '5842555','3174333572', 'CUCUTA','true', '', '', '', '', 26],
-	['Farmanorte 26', 7.87187112, -72.52892628, 'Avenida 26 No 29-48 Br Belen', '5754140','3162331144', 'CUCUTA','','7:30', '21:30', '7:30', '7:30', 27],
-	['Farmanorte 27', 7.91694492, -72.4727475, 'El Escobal, Anillo Vial, Plaza Del Este Local 6', '5847808','3188135356', 'CUCUTA','','7', '22', '7', '7', 28],
-	['San Antonio del Norte', 7.88749215, -72.50609315, 'Av 7 Calle 9 Esquina Centro', '5727091','3155997098', 'CUCUTA','','7', '20', '8', '8', 29],
-
-
+	['Farmanorte 26', 7.87187112, -72.52892628, 'Avenida 26 No 29-48 Br Belen', '5754140','3162331144', 'CUCUTA','','7:30am', '21:30', '7:30am', '9:30pm', 27],
+	['Farmanorte 27', 7.91694492, -72.4727475, 'El Escobal, Anillo Vial, Plaza Del Este Local 6', '5847808','3188135356', 'CUCUTA','','7am', '22', '7am', '10pm', 28],
+	['San Antonio del Norte', 7.88749215, -72.50609315, 'Av 7 Calle 9 Esquina Centro', '5727091','3155997098', 'CUCUTA','','7am', '20', '8am', '2pm', 29],
 
 ];
 
@@ -141,7 +139,7 @@ function addMarkerWithTimeout(position, timeout, suc, i, dir, telefono, celular,
 						
 		var complementoHora = 	'<div class="contentestado">'+
 									'<div class="titleestado"><h4>Estado</h4></div>'+
-									'<div class="infoestado"><span class="icon-primitive-dot"></span>&nbsp;Cerrado</div>'+
+									'<div class="infoestado"><i id="iconestado" class="zmdi zmdi-circle"></i>&nbsp;<span id="estadosuc"></span></div>'+
 								'</div>'+
 							'</div><!-- fin de layoutcontentbutton de horarios-->'+
 						'</div><!-- fin de layoutcontent-->';
@@ -160,21 +158,42 @@ function addMarkerWithTimeout(position, timeout, suc, i, dir, telefono, celular,
             '</div>';
 
 
-        //necesitamos obtener el dia de la semana
-        var fechaActual = new Date();
-        var ahora = fechaActual.getHours();
-        var elDia = fechaActual.getDay();
-
-        console.log("Es 24 horas: " + new Boolean(_24H).value);
-        console.log(new Boolean(_24H));
+        
         //con base en el dia comparamos el rango de horas icluyendo minutos
         //genero el contenido dinamicamente
         
-        if( new Boolean(_24H)){
+        if( _24H === 'true'){
         	contents +=   _24_horas + complementoHora + footer ;
         }else{        	
-        	// contents +=  hOrdinario + complementoHora + footer ;
-        }
+        	var fechaActual = new Date();
+        	var horaActual = fechaActual.getHours();
+        	var diaDeLaSemana = fechaActual.getDay();
+
+        	var horaApertura = new Date();
+        	var horaCierre = new Date();
+        	//valida si el dia actual esta entre lunes y Sabado
+			if (diaDeLaSemana >= 0 && diaDeLaSemana <= 5) {
+				//valida que la hora actual este dentro del horario de la sucursal	
+				//Parsea las horas recibidas del arreglo a formato hora JavaScript
+				var time = aLV.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+				horaApertura.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
+				horaApertura.setMinutes(parseInt(time[2]) || 0);
+				
+				var time = cLV.match(/(\d+)(?::(\d\d))?\s*(p?)/);
+				horaCierre.setHours(parseInt(time[1]) + (time[3] ? 12 : 0));
+				horaCierre.setMinutes(parseInt(time[2]) || 0);				
+
+				// console.log("Sucursal " + suc + " " + "Abierto: " + (fechaActual.getHours() >= horaApertura.getHours()) );
+				contents += hOrdinario + complementoHora + footer;
+				// if( (fechaActual.getHours() >= horaApertura.getHours()) && (fechaActual.getHours()<=horaCierre.getHours()) ){
+				// 	document.getElementById('iconestado').setAttribute("style","zmdi zmdi-circle iconestadoabierto");
+				// 	document.getElementById('estadosuc').appendChild(document.createTextNode("Abierto"));
+				// }else{
+				// 	document.getElementById('iconestado').setAttribute("style","zmdi zmdi-circle iconestadocerrado");
+				// 	document.getElementById('estadosuc').appendChild(document.createTextNode("Cerrado"));
+				// }
+			}//fin del if de horario
+        }//fin del else
 
 
 		//registro del manejo de evento click para desplegar el objeto InfoWindow
@@ -197,6 +216,17 @@ function addMarkerWithTimeout(position, timeout, suc, i, dir, telefono, celular,
 			}, i * 50);
 }
 
+//formato de hora 
+function formatAMPM(date) {
+	var hours = date.getHours();
+	var minutes = date.getMinutes();
+	var ampm = hours >= 12 ? 'pm' : 'am';
+	hours = hours % 12;
+	hours = hours ? hours : 12; // the hour '0' should be '12'
+	minutes = minutes < 10 ? '0' + minutes : minutes;
+	var strTime = hours + ':' + minutes + ' ' + ampm;
+	return strTime;
+}
 
 //anade el marcardor "Marker" al mapa y registra el evento click sobre el marcador
 //para mostrar la informacion de la sucursal en un objeto InfoWindow
